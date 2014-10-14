1e4b618b3d5600eba0aaa380406d934de7d4da64
davemfish <davemfish@gmail.com>
2014-10-14 12:20:35 -0700

    remove aoi.shp to geojson. leaflet no longer depends on aoi to set map view.

--- utils/shell_post.sh
+++ utils/shell_post.sh
@@ -1,18 +1,17 @@
 
 ## grid_private.dbf did not include the PUD values, workaround:
-cp grid_private.shp grid.shp
-cp grid_private.prj grid.prj
-cp grid_private.shx grid.shx
-cp grid_public.dbf grid.dbf
+sudo cp grid_private.shp grid.shp
+sudo cp grid_private.prj grid.prj
+sudo cp grid_private.shx grid.shx
+sudo cp grid_public.dbf grid.dbf
 
 ## shapefile to geoJSON and transform to wgs84
-ogr2ogr -f GeoJSON -t_srs crs:84 grid.geojson grid.shp 
+sudo ogr2ogr -f GeoJSON -t_srs crs:84 grid.geojson grid.shp 
 
 ## dbf to csv because foreign::read.dbf() does not like a url pathname
-ogr2ogr -f CSV grid.csv grid_public.dbf
+sudo ogr2ogr -f CSV grid.csv grid_public.dbf
 
 ## aoi shapefile to geojson, transform, and write bbox
 ## may need to generalize this by first dissolving aoi.shp, in case it is multipart.
 ## WRITE_BBOX ought to create bbox for the entire layer, but is only creating bbox for individual polygons.
-ogr2ogr -f GeoJSON -t_srs crs:84 -lco WRITE_BBOX=YES aoi.geojson aoi.shp 
-
+## sudo ogr2ogr -f GeoJSON -t_srs crs:84 -lco WRITE_BBOX=YES aoi.geojson aoi.shp 
