68ef11b0c68c5fb805248e0f4978a09583dd3582
davemfish <davemfish@gmail.com>
2014-10-13 17:27:21 -0700

    includes functions for loading log and workspace. These were abstracted to facilitate loading 2 workspaces at once for the Comparison Tab.

--- app-rec/global.r
+++ app-rec/global.r
@@ -1,14 +1,39 @@
+getLog <- function(x){
+  logfile <- readLines(con=file.path(x), n=-1)
+  blanks <- which(logfile=="")
+  logtable <- logfile[1:(min(blanks) - 1)]
+  sessionline <- logfile[grep(logfile, pattern="Assigned server session id")]
+  sessid <- sub(sessionline, pattern=".*Assigned server session id ", replacement="")
+  sessid <- sub(sessid, pattern="\\.", replacement="")
+  return(list(sessid=sessid, logtable=logtable))
+}
+
+getSpace <- function(x, two=FALSE){ # x is the session ID
+  ws <- file.path("http://ncp-skookum.stanford.edu/~woodsp", x)
+  #unzip(Sys.glob(file.path(ws, "results*.zip")), exdir=ws, overwrite=T)
+  atts <- read.csv(file.path(ws, "grid.csv"))
+  geom <- fromJSON(file.path(ws, "grid.geojson"))
+  aoijson <- fromJSON(file.path(ws, "aoi.geojson"))
+  aoibbox <- aoijson[[2]][[1]]$bbox
+  center <- c(mean(c(aoibbox[2], aoibbox[4])), mean(c(aoibbox[1], aoibbox[3])))    
+  zoom <- 8
+  view <- list(center=center, zoom=zoom)
+  if (two == FALSE){
+    return(list(atts=atts, geom=geom, view=view))
+  } else {
+    return(list(atts=atts))
+  }
+}
+
+
+
+
 Cut2Num <- function(x){
   ids <- unique(as.numeric(x))
   char.x <- as.character(levels(x))
   num.x <- as.numeric(gsub(unlist(strsplit(char.x, split=",")), pattern='\\(|\\]', replacement=""))
   return(list(brks=unique(num.x), ids=ids))
 }
-
-# recTemplate <- "<script>
-#                 new L.Control.Zoom({ position: 'topright' }).addTo(map);
-#                 </script>"
-
 
 ### a mod of rCharts::renderMap to add on custom javascript to the 
 ### HTML that shiny ui will render. The js should be leaflet and 
